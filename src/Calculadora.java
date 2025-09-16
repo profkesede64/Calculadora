9137b2e6d01d9dbc5b7aa4e7ffdfce14852f9520
profkesede <profkesede64@gmail.com>
2025-09-16 16:16:36 -0300

    Adiciona método de subtração

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,7 +2,12 @@ public class Calculadora {
     public static int soma(int a, int b) {
         return a + b;
     }
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Resultado: " + soma(5, 3));
+        System.out.println("Resultado da subtração: " + subtracao(5, 3));
     }
 }
